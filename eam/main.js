5a1494e607aa9d438cfeda78c9828c1900f633f7
Panos Evange <panosevage@yahoo.com>
2018-11-28 06:33:03 +0200

    added showPassword with eye icon

--- eam/main.js
+++ eam/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popover"]').popover({html:true});
 });
 
 $(function(){
@@ -22,3 +22,49 @@ $(function(){
       })
   })
 })
+
+$(document).ready(function() {
+    $("#show_hide_password_first a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password_first input').attr("type") == "text"){
+            $('#show_hide_password_first input').attr('type', 'password');
+            $('#show_hide_password_first i').addClass( "fa-eye-slash" );
+            $('#show_hide_password_first i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password_first input').attr("type") == "password"){
+            $('#show_hide_password_first input').attr('type', 'text');
+            $('#show_hide_password_first i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password_first i').addClass( "fa-eye" );
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#show_hide_password_confirm a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password_confirm input').attr("type") == "text"){
+            $('#show_hide_password_confirm input').attr('type', 'password');
+            $('#show_hide_password_confirm i').addClass( "fa-eye-slash" );
+            $('#show_hide_password_confirm i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password_confirm input').attr("type") == "password"){
+            $('#show_hide_password_confirm input').attr('type', 'text');
+            $('#show_hide_password_confirm i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password_confirm i').addClass( "fa-eye" );
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    $("#show_hide_password_login a").on('click', function(event) {
+        event.preventDefault();
+        if($('#show_hide_password_login input').attr("type") == "text"){
+            $('#show_hide_password_login input').attr('type', 'password');
+            $('#show_hide_password_login i').addClass( "fa-eye-slash" );
+            $('#show_hide_password_login i').removeClass( "fa-eye" );
+        }else if($('#show_hide_password_login input').attr("type") == "password"){
+            $('#show_hide_password_login input').attr('type', 'text');
+            $('#show_hide_password_login i').removeClass( "fa-eye-slash" );
+            $('#show_hide_password_login i').addClass( "fa-eye" );
+        }
+    });
+});
