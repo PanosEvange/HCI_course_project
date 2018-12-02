4935ba8d1e6876514f5b3205cefd3600ce7ffa13
Panos Evange <panosevage@yahoo.com>
2018-12-02 05:08:42 +0200

    initial registration before update db

--- eam/registration_form.php
+++ eam/registration_form.php
@@ -37,113 +37,182 @@
 
         </div>
 
+        <?php
+            // define variables and set to empty values
+            $emailErr = $passErr = "";
+            $email = $pass = "";
+            $succ = "";
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+              if (empty($_POST["password"])) {
+                $passErr = "Password is required";
+              } else {
+                $pass = test_input($_POST["password"]);
+                // check if name only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z]*$/",$pass)) {
+                  $passErr = "Only letters allowed";
+                }
+              }
+
+              if (empty($_POST["email"])) {
+                $emailErr = "Email is required";
+              } else {
+                $email = test_input($_POST["email"]);
+                // check if e-mail address is well-formed
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                  $emailErr = "Invalid email format";
+                }
+
+                if( ! (strcmp($emailErr, "") or strcmp($emailErr, "")) ){
+                    //save to db
+                    
+                    $succ = "Επιτυχής καταχώρηση!";
+                }
+
+              }
+            }
+
+            function test_input($data) {
+              $data = trim($data);
+              $data = stripslashes($data);
+              $data = htmlspecialchars($data);
+              return $data;
+            }
+
+        ?>
+
         <div class="my-main-content-registration">
             <p> Η συμπλήρωση των πεδίων με <span class="my-req-star">*</span> είναι υποχρεωτική.</p>
-            <div class="my-reg-account-info">
-                <h> Στοιχεία Λογαριασμού </h>
-                <form>
-                  <div class="form-group row">
-                    <label for="myEmail" class="col-5 col-form-label"><i class="fa fa-envelope" aria-hidden="true"></i>   <span class="my-req-star">*</span>Email:</label>
-                    <div class="col-6">
-                      <input type="text" class="form-control" id="myEmail">
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="myPassword" class="col-5 col-form-label"><i class="fa fa-lock" aria-hidden="true"></i>   <span class="my-req-star">*</span>Κωδικός:</label>
-                    <div class="col-6">
-                        <div class="input-group" id="show_hide_password_first">
-                          <input class="form-control" type="password" id="myPassword">
-                          <div class="input-group-addon">
-                            <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
-                          </div>
+            <p class="success-registration"> <?php echo $succ;?> </p>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="my-registration-from">
+                <div class="my-reg-account-info">
+                    <h> Στοιχεία Λογαριασμού </h>
+                      <div class="form-group row">
+                        <label for="myEmail" class="col-5 col-form-label"><i class="fa fa-envelope" aria-hidden="true"></i>   <span class="my-req-star">*</span>Email:</label>
+                        <div class="col-6">
+                          <input type="text" name="email" value="<?php echo $email;?>" class="form-control" id="myEmail">
                         </div>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="myPasswordConfirm" class="col-5 col-form-label"><i class="fa fa-lock" aria-hidden="true"></i>   <span class="my-req-star">*</span>Επιβεβαίωση:</label>
-                    <div class="col-6">
-                        <div class="input-group" id="show_hide_password_confirm">
-                          <input class="form-control" type="password" id="myPasswordConfirm">
-                          <div class="input-group-addon">
-                            <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                      </div>
+                      <div class="row error-msg">
+                          <div class="col-5">
                           </div>
+                          <div class="col-6">
+                              <?php echo $emailErr;?>
+                          </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="myPassword" class="col-5 col-form-label"><i class="fa fa-lock" aria-hidden="true"></i>   <span class="my-req-star">*</span>Κωδικός:</label>
+                        <div class="col-6">
+                            <div class="input-group" id="show_hide_password_first">
+                              <input class="form-control" name="password" value="<?php echo $pass;?>" type="password" id="myPassword">
+                              <div class="input-group-addon">
+                                <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                              </div>
+                            </div>
                         </div>
-                      <!-- <input type="password" class="form-control" id="myPasswordConfirm" placeholder="Password"> -->
+                      </div>
+                      <div class="row error-msg">
+                          <div class="col-5">
+                          </div>
+                          <div class="col-6">
+                              <?php echo $passErr;?>
+                          </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="myPasswordConfirm" class="col-5 col-form-label"><i class="fa fa-lock" aria-hidden="true"></i>   <span class="my-req-star">*</span>Επιβεβαίωση:</label>
+                        <div class="col-6">
+                            <div class="input-group" id="show_hide_password_confirm">
+                              <input class="form-control" type="password" id="myPasswordConfirm">
+                              <div class="input-group-addon">
+                                <a href=""><i class="fa fa-eye-slash" aria-hidden="true"></i></a>
+                              </div>
+                            </div>
+                          <!-- <input type="password" class="form-control" id="myPasswordConfirm" placeholder="Password"> -->
+                        </div>
+                      </div>
+                </div>
+                <div class="my-reg-personal-info">
+                    <h> Προσωπικά Στοιχεία </h>
+                    <div class="form-group row">
+                      <label for="myFirstName" class="col-5 col-form-label"><span class="my-req-star">*</span>Όνομα:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myFirstName">
+                      </div>
                     </div>
-                  </div>
-                </form>
-            </div>
-            <div class="my-reg-personal-info">
-                <h> Προσωπικά Στοιχεία </h>
-                <div class="form-group row">
-                  <label for="myFirstName" class="col-5 col-form-label"><span class="my-req-star">*</span>Όνομα:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myFirstName">
-                  </div>
+                    <div class="form-group row">
+                      <label for="myLastName" class="col-5 col-form-label"><span class="my-req-star">*</span>Επώνυμο:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myLastName">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myBirthDate" class="col-5 col-form-label"><span class="my-req-star">*</span>Ημερομηνία Γέννησης:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myBirthDate">
+                      </div>
+                    </div>
                 </div>
-                <div class="form-group row">
-                  <label for="myLastName" class="col-5 col-form-label"><span class="my-req-star">*</span>Επώνυμο:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myLastName">
-                  </div>
+                <div class="my-reg-law-info">
+                    <h> Νομικά Στοιχεία </h>
+                    <div class="form-group row">
+                      <label for="myIdNumber" class="col-5 col-form-label"><span class="my-req-star">*</span>Αριθμός Ταυτότητας:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myIdNumber">
+                      </div>
+                      <div class="col-1">
+                          <span class="my-question-popover" title="" data-toggle="popover" data-trigger="hover"
+                          data-content="Εισάγετε τον αριθμό που αναγράφεται στην μπροστινή μεριά της αστυνομικής ταυτότητας σας. Ξεκινάει υποχρεωτικά
+                          με Α ακολουθούμενο από ένα γράμμα και αριθμούς ή μόνο αριθμούς. Απαραίτητα 10 χαρακτήρες.">
+                              <i class="fa fa-question-circle" aria-hidden="true"></i></span>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myTaxNumber" class="col-5 col-form-label"><span class="my-req-star">*</span>ΑΦΜ:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myTaxNumber">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myAmkaNumber" class="col-5 col-form-label"><span class="my-req-star">*</span>ΑΜΚΑ:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myAmkaNumber">
+                      </div>
+                    </div>
                 </div>
-                <div class="form-group row">
-                  <label for="myBirthDate" class="col-5 col-form-label"><span class="my-req-star">*</span>Ημερομηνία Γέννησης:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myBirthDate">
-                  </div>
+                <div class="my-reg-contact-info">
+                    <h> Στοιχεία Επικοινωνίας </h>
+                    <div class="form-group row">
+                      <label for="myPhoneNumber" class="col-5 col-form-label"><i class="fa fa-phone" aria-hidden="true"></i> Τηλέφωνο Επικοινωνίας:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myPhoneNumber">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myAddress" class="col-5 col-form-label">Διεύθυνση Κατοικίας:</label>
+                      <div class="col-6">
+                        <input type="text" class="form-control" id="myAddress">
+                      </div>
+                    </div>
                 </div>
-            </div>
-            <div class="my-reg-law-info">
-                <h> Νομικά Στοιχεία </h>
-                <div class="form-group row">
-                  <label for="myIdNumber" class="col-5 col-form-label"><span class="my-req-star">*</span>Αριθμός Ταυτότητας:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myIdNumber">
-                  </div>
-                  <div class="col-1">
-                      <span class="my-question-popover" title="" data-toggle="popover" data-trigger="hover"
-                      data-content="Εισάγετε τον αριθμό που αναγράφεται στην μπροστινή μεριά της αστυνομικής ταυτότητας σας. Ξεκινάει υποχρεωτικά
-                      με Α ακολουθούμενο από ένα γράμμα και αριθμούς ή μόνο αριθμούς. Απαραίτητα 10 χαρακτήρες.">
-                          <i class="fa fa-question-circle" aria-hidden="true"></i></span>
-                  </div>
+                <div class="my-registration-submit">
+                    <div class="row">
+                        <label><input type="checkbox" value="">  Έχω διαβάσει και αποδέχομαι τους <a href="./under_construction.php">όρους χρήσης</a> </label>
+                    </div>
+                    <div class="row">
+                        <input type="submit" name="submit" class="btn btn-primary" value="Ολοκλήρωση Εγγραφής">
+                    </div>
+
                 </div>
-                <div class="form-group row">
-                  <label for="myTaxNumber" class="col-5 col-form-label"><span class="my-req-star">*</span>ΑΦΜ:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myTaxNumber">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="myAmkaNumber" class="col-5 col-form-label"><span class="my-req-star">*</span>ΑΜΚΑ:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myAmkaNumber">
-                  </div>
-                </div>
-            </div>
-            <div class="my-reg-contact-info">
-                <h> Στοιχεία Επικοινωνίας </h>
-                <div class="form-group row">
-                  <label for="myPhoneNumber" class="col-5 col-form-label"><i class="fa fa-phone" aria-hidden="true"></i> Τηλέφωνο Επικοινωνίας:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myPhoneNumber">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="myAddress" class="col-5 col-form-label">Διεύθυνση Κατοικίας:</label>
-                  <div class="col-6">
-                    <input type="text" class="form-control" id="myAddress">
-                  </div>
-                </div>
-            </div>
+            </form>
         </div>
 
-        <div class="my-registration-submit">
+        <!-- <div class="my-registration-submit">
             <div class="checkbox">
-              <label><input type="checkbox" value="">  Έχω διαβάσει και αποδέχομαι τους <a href="./under_construction.php">όρους χρήσης</a> </label>
+
             </div>
             <a href="./under_construction.php" class="btn btn-primary"> Ολοκλήρωση Εγγραφής </a>
-        </div>
+        </div> -->
 
 
         <!-- Footer Start  -->
