25a7c23ed3b19aeb57f5deeb7dbbb1cfcb3e2b40
Panos Evange <panosevage@yahoo.com>
2018-12-04 05:33:49 +0200

    minor refactoring signup.php

--- eam/signup.php
+++ eam/signup.php
@@ -1,19 +1,8 @@
 <?php
     include 'login_db.php';
     // define variables and set to empty values
-    $emailErr = $passErr = $passConfErr = $firstNameErr = $lastNameErr = $dateErr = $idErr = $amkaErr = $taxErr = $addressErr = $phoneErr = "";
     $email = $pass = $passConf = $firstName = $lastName = $date = $id = $amka = $tax = $address = $phone = "";
     $succ = "";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    $stmt = $conn->prepare("INSERT INTO Publisher (Email, Password, LastName,
-                            FirstName, DateOfBirth, IdNum, TaxNum, AMKA, Phone,
-                             Address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssssss", $email, $pass, $firstName,
-                        $lastName, $date, $id, $tax, $amka, $phone, $address);
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -31,6 +20,15 @@
 
         require_once 'login_db.php'; //db info
 
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $stmt = $conn->prepare("INSERT INTO Publisher (Email, Password, LastName,
+                                FirstName, DateOfBirth, IdNum, TaxNum, AMKA, Phone,
+                                 Address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssssssss", $email, $pass, $firstName,
+                            $lastName, $date, $id, $tax, $amka, $phone, $address);
 
         $stmt->execute();
 
