6b279a7984a730b5e2e627a291e389240bebdf4e
Panos Evange <panosevage@yahoo.com>
2018-12-05 07:40:06 +0200

    initial version of student_profile

--- eam/my_profile_student.php
+++ eam/my_profile_student.php
@@ -36,7 +36,113 @@
 
         </div>
 
-        <div class="placeholder">
+        <div class="my-main-content-registration">
+
+                <div class="my-reg-account-info">
+                    <h> Στοιχεία Λογαριασμού </h>
+                      <div class="form-group row">
+                        <label for="myEmail" class="col-5 col-form-label"><i class="fa fa-envelope" aria-hidden="true"></i>   <span class="my-req-star">*</span>Email:</label>
+                        <div class="col-6">
+                            Test
+                        </div>
+                        <div class="col-1">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="myPassword" class="col-5 col-form-label"><i class="fa fa-lock" aria-hidden="true"></i>   <span class="my-req-star">*</span>Κωδικός:</label>
+                        <div class="col-6">
+                            Test
+                        </div>
+                        <div class="col-1">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="myPasswordConfirm" class="col-5 col-form-label"><i class="fa fa-lock" aria-hidden="true"></i>   <span class="my-req-star">*</span>Επιβεβαίωση:</label>
+                        <div class="col-6">
+                            <div class="input-group" id="show_hide_password_confirm">
+                                Test
+                            </div>
+                        </div>
+                        <div class="col-1">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </div>
+                      </div>
+                </div>
+                <div class="my-reg-personal-info">
+                    <h> Προσωπικά Στοιχεία </h>
+                    <div class="form-group row">
+                      <label for="myFirstName" class="col-5 col-form-label"><span class="my-req-star">*</span>Όνομα:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myLastName" class="col-5 col-form-label"><span class="my-req-star">*</span>Επώνυμο:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myBirthDate" class="col-5 col-form-label"><span class="my-req-star">*</span>Ημερομηνία Γέννησης:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                </div>
+                <div class="my-university-info">
+                    <h> Στοιχεία Σχολής </h>
+                    <div class="form-group row">
+                      <label for="myUniv" class="col-5 col-form-label"><span class="my-req-star">*</span>Σχολή:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myDepart" class="col-5 col-form-label"><span class="my-req-star">*</span>Τμήμα:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                </div>
+                <div class="my-reg-contact-info">
+                    <h> Στοιχεία Επικοινωνίας </h>
+                    <div class="form-group row">
+                      <label for="myPhoneNumber" class="col-5 col-form-label"><i class="fa fa-phone" aria-hidden="true"></i> Τηλέφωνο Επικοινωνίας:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="myAddress" class="col-5 col-form-label">Διεύθυνση Κατοικίας:</label>
+                      <div class="col-6">
+                          Test
+                      </div>
+                      <div class="col-1">
+                          <i class="fa fa-pencil" aria-hidden="true"></i>
+                      </div>
+                    </div>
+
+                </div>
 
         </div>
 
