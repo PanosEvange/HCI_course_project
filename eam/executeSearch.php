ee816414dfe8b0e7c840a4b2673a52e0aa72c563
Vag_Spithas <sdi1500147@di.uoa.gr>
2018-12-12 23:34:37 +0200

    Search implemented only for book title, No filters. Shows up to 10 results.

--- eam/executeSearch.php
+++ eam/executeSearch.php
@@ -1,125 +1,87 @@
 <?php
 
+    include 'login_db.php';
+
     if (isset($_GET['q'])) {
 
         $searchTerm = $_GET['q'];
 
-        echo '
-            <div class="mySearchBookResultsCount">
-                Βρέθηκαν <span class="mySearchBookCounter">10</span> αποτελέσματα για \''.$searchTerm.'\'.
-            </div>
-        ';
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-        echo '
-            <div class="mySearchBookOneResult">
-                <div class="book-image">
-                    <img src="./150.png" alt="Image Placeholder">
-                </div>
-                <div class="book-title">
-                    Διακριτά Μαθηματικά για Χημικούς
-                </div>
-                <div class="book-authors">
-                    Συγγραφείς: Μάστορας Γιώργος, Σπάθας Μιχάλης
-                </div>
-                <div class="book-publisher">
-                    Εκδόσεις: Αλχημεία
-                </div>
-                <div class="book-isbn">
-                    ISBN: 123666123
-                </div>
-                <div class="book-comments">
-                    <a href="#">Σχόλια(5)</a>
-                </div>
-                <div class="book-page">
-                    <a href="./under_construction.php"> Σελίδα του Βιβλίου <i class="fa fa-chevron-right" aria-hidden="true"></i> </a>
-                </div>
-            </div>
+        $sql = ("SELECT * FROM Books WHERE Name LIKE '%$searchTerm%'");
+        $result = $conn->query($sql);
+        $rows = $result->num_rows;
 
-            <div class="mySearchBookOneResult">
-                <div class="book-image">
-                    <img src="./150.png" alt="Image Placeholder">
+        if ($rows > 0) {
+            echo '
+                <div class="mySearchBookResultsCount">
+                Βρέθηκαν <span class="mySearchBookCounter">'.$rows.'</span> αποτελέσματα για \''.$searchTerm.'\'.
                 </div>
-                <div class="book-title">
-                    Διακριτά Μαθηματικά για Χημικούς
+            ';
+            while ($rows != 0) {
+                if ($rows > 10) {
+                    $page_res = 10;
+                    $rows = $rows - 10;
+                } else {
+                    $page_res = $rows;
+                    $rows = 0;
+                }
+                for ($i=0; $i < $page_res; $i++) {
+                    $row = $result->fetch_assoc();
+                    // if (isset($_GET["myIsbn-filter"])) {
+                    //     if ($row["ISBN"] != $_GET["myIsbn-filter"]) {
+                    //         continue;
+                    //     }
+                    // }
+                    echo '
+                            <div class="mySearchBookOneResult">
+                                <div class="book-image">
+                                    <img src="./150.png" alt="Image Placeholder">
+                                </div>
+                                <div class="book-title">'.$row["Name"].'
+                                </div>
+                                <div class="book-authors">'.$row["Author"].'
+                                </div>
+                                <div class="book-publisher">
+                                    Εκδόσεις: '.$row["Publisher"].'
+                                </div>
+                                <div class="book-isbn">
+                                    ISBN: '.$row["ISBN"].'
+                                </div>
+                                <div class="book-comments">
+                                    <a href="#">Σχόλια(5)</a>
+                                </div>
+                                <div class="book-page">
+                                    <a href="./under_construction.php"> Σελίδα του Βιβλίου <i class="fa fa-chevron-right" aria-hidden="true"></i> </a>
+                                </div>
+                            </div>
+                        ';
+                }
+                break;
+            }
+        }
+        else {
+            echo '
+                <div class="mySearchBookResultsCount">
+                Δρν βρέθηκαν αποτελέσματα για \''.$searchTerm.'\'.
                 </div>
-                <div class="book-authors">
-                    Συγγραφείς: Μάστορας Γιώργος, Σπάθας Μιχάλης
-                </div>
-                <div class="book-publisher">
-                    Εκδόσεις: Αλχημεία
-                </div>
-                <div class="book-isbn">
-                    ISBN: 123666123
-                </div>
-                <div class="book-comments">
-                    <a href="#">Σχόλια(5)</a>
-                </div>
-                <div class="book-page">
-                    <a href="./under_construction.php"> Σελίδα του Βιβλίου <i class="fa fa-chevron-right" aria-hidden="true"></i> </a>
-                </div>
-            </div>
+            ';
+        }
 
-            <div class="mySearchBookOneResult">
-                <div class="book-image">
-                    <img src="./150.png" alt="Image Placeholder">
+        echo '<div class="endOfResultsPlaceHolder">
                 </div>
-                <div class="book-title">
-                    Διακριτά Μαθηματικά για Χημικούς
-                </div>
-                <div class="book-authors">
-                    Συγγραφείς: Μάστορας Γιώργος, Σπάθας Μιχάλης
-                </div>
-                <div class="book-publisher">
-                    Εκδόσεις: Αλχημεία
-                </div>
-                <div class="book-isbn">
-                    ISBN: 123666123
-                </div>
-                <div class="book-comments">
-                    <a href="#">Σχόλια(5)</a>
-                </div>
-                <div class="book-page">
-                    <a href="./under_construction.php"> Σελίδα του Βιβλίου <i class="fa fa-chevron-right" aria-hidden="true"></i> </a>
-                </div>
-            </div>
+            ';
 
-            <div class="mySearchBookOneResult">
-                <div class="book-image">
-                    <img src="./150.png" alt="Image Placeholder">
-                </div>
-                <div class="book-title">
-                    Διακριτά Μαθηματικά για Χημικούς
-                </div>
-                <div class="book-authors">
-                    Συγγραφείς: Μάστορας Γιώργος, Σπάθας Μιχάλης
-                </div>
-                <div class="book-publisher">
-                    Εκδόσεις: Αλχημεία
-                </div>
-                <div class="book-isbn">
-                    ISBN: 123666123
-                </div>
-                <div class="book-comments">
-                    <a href="#">Σχόλια(5)</a>
-                </div>
-                <div class="book-page">
-                    <a href="./under_construction.php"> Σελίδα του Βιβλίου <i class="fa fa-chevron-right" aria-hidden="true"></i> </a>
-                </div>
-            </div>
-
-            <div class="endOfResultsPlaceHolder">
-
-            </div>
-        ';
+        $conn->close();
 
     }
     else {
         //do nothing
-        // echo "You haven't search anything yet!";
         echo '
             <div class="placeholder"></div>
         ';
     }
-
-
 ?>
