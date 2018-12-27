8b18412ab30e6722870e670a8455467ca81acc8a
Panos Evange <panosevage@yahoo.com>
2018-12-27 17:44:58 +0200

    userOptions initial version

--- eam/userOptions.php
+++ eam/userOptions.php
@@ -1,17 +1,18 @@
 <div id="accordion" class="user-options">
   <div class="card">
+      <div id="collapseOne" class="collapse" data-parent="#accordion">
+        <div class="card-body user-options-buttons">
+            <a href="./under_construction.php" class="btn"> Δήλωση Συγγραμμάτων </a>
+            <a href="./under_construction.php" class="btn my-ml"> Ανταλλαγή Συγγραμμάτων </a>
+            <a href="./student_app.php" class="btn my-ml"> Εφαρμογή Φοιτητών </a>
+            <a href="./under_construction.php" class="btn my-ml"> Πληροφορίες για Φοιτητές </a>
+        </div>
+      </div>
     <div class="card-header">
       <a class="card-link" data-toggle="collapse" href="#collapseOne">
         Οι επιλογές μου   <i class="fa fa-angle-down" aria-hidden="true"></i>
       </a>
     </div>
-    <div id="collapseOne" class="collapse show" data-parent="#accordion">
-      <div class="card-body user-options-buttons">
-          <a href="./under_construction.php" class="btn"> Δήλωση Συγγραμμάτων </a>
-          <a href="./under_construction.php" class="btn my-ml"> Ανταλλαγή Συγγραμμάτων </a>
-          <a href="./student_app.php" class="btn my-ml"> Εφαρμογή Φοιτητών </a>
-          <a href="./under_construction.php" class="btn my-ml"> Πληροφορίες για Φοιτητές </a>
-      </div>
-    </div>
+
   </div>
 </div>
