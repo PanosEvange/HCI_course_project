ad7ab8913b52850a36ee93761d53a52978de1e92
spithas <vaggelisspithas@gmail.com>
2018-12-30 19:25:15 +0200

    Autocomplete for title completed

--- eam/executeSearch.php
+++ eam/executeSearch.php
@@ -86,11 +86,10 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
     if (!$conn->set_charset("utf8")) {
         // printf("Error loading character set utf8: %s<br>", $conn->error);
         exit();
-    } else {
-        // printf("Current character set: %s<br>", $conn->character_set_name());
     }
 
     if (isset($_REQUEST['q'])) {
